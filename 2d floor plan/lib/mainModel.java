fa9c68712a27183ebbfec4fced97dfd3d7ed6363
satvik-A <satvikaderla@gmail.com>
2024-11-04 23:27:26 +0530

    Update mainModel.java

--- 2d floor plan/lib/mainModel.java
+++ 2d floor plan/lib/mainModel.java
@@ -18,7 +18,7 @@ class mainModel {
         JPanel panelSIDE = new JPanel();
         panelSIDE.setBackground(Color.YELLOW);
         panelSIDE.setPreferredSize(new Dimension(150, 980));
-        panelSIDE.setLayout(new BorderLayout());
+        panelSIDE.setLayout(new GridLayout(10, 1, 0, 0));
         m.f.add(panelSIDE, BorderLayout.WEST);
 
         // INSTRUMENT PANNEL / TOPBAR /
@@ -27,7 +27,7 @@ class mainModel {
         paneltop.setPreferredSize(new Dimension(100, 100));
         m.f.add(paneltop, BorderLayout.NORTH);
         // layout = = grid layout
-        paneltop.setLayout(new GridLayout(0, 10, 3, 3));
+        paneltop.setLayout(new GridLayout(1, 10, 3, 3));
         // for horizontal buttons
 
         // DRAWING BORD /
@@ -40,29 +40,15 @@ class mainModel {
         JPanel panelSIDE1 = new JPanel();
         panelSIDE1.setBackground(Color.RED);
         panelSIDE1.setPreferredSize(new Dimension(150, 100));
-        panelSIDE.add(panelSIDE1, BorderLayout.NORTH);
+        panelSIDE.add(panelSIDE1);
         //
 
         //
         JPanel panelSIDE4 = new JPanel();
-        panelSIDE4.setBackground(Color.RED);
+        panelSIDE4.setBackground(Color.BLACK);
         panelSIDE4.setPreferredSize(new Dimension(150, 100));
         panelSIDE.add(panelSIDE4, BorderLayout.SOUTH);
         //
-
-        // inside action listener for side panal object creator like creation of new
-        // rooms
-
-        JPanel panelSIDE2 = new JPanel();
-        panelSIDE2.setBackground(Color.CYAN);
-        panelSIDE2.setPreferredSize(new Dimension(150, 100));
-        panelSIDE2.setLayout(new BorderLayout());
-        panelSIDE.add(panelSIDE2, BorderLayout.CENTER);
-
-        JPanel panelSIDE3 = new JPanel();
-        panelSIDE3.setBackground(Color.DARK_GRAY);
-        panelSIDE3.setPreferredSize(new Dimension(150, 100));
-        panelSIDE2.add(panelSIDE3, BorderLayout.NORTH);
 
     }
 
